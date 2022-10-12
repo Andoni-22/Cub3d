9da706e00b51ffc0d533bc0d5891d56b47de416c
Lukas Gonzalez <lukas@techfriendly.es>
2022-10-12 17:35:36 +0200

    first Makefile

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "libft.h"
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	printf("Hello World\n");
+	printf("MY ATOI: %d\n", ft_atoi("123"));
+	return (0);
+}
